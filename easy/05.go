fe1a1c0d58eed6ff0f2ea1289b8976c8cfff76a7
mianmianyixia <1608431986@qq.com>
2025-10-31 13:46:20 +0800

    方法的使用

---
+++ easy/05.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	var person Person
+	fmt.Println("输入姓名")
+	fmt.Scan(&person.name)
+	fmt.Println("请输入年龄")
+	fmt.Scan(&person.age)
+	person.Greet()
+}
+func (person *Person) Greet() {
+	fmt.Printf("你好，我是%s，今年%d", person.name, person.age)
+}
+
+type Person struct {
+	name string
+	age  int
+}
