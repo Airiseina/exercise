f4a3775e78a30056437192c8f05e38854b2befb1
mianmianyixia <1608431986@qq.com>
2025-11-02 00:15:38 +0800

    接口的用法

---
+++ easy/06.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+func main() {
+	myPrintLn(3)
+	myPrintLn("你好世界")
+	myPrintLn([]int{1, 2, 3})
+	myPrintLn([]string{"你好", "世界"})
+}
+func myPrintLn(input ...any) {
+	for _, shuju := range input {
+		if ints, ok := shuju.(int); ok {
+			fmt.Printf("ints: %d\n", ints)
+			continue
+		}
+		if strings, ok := shuju.(string); ok {
+			fmt.Printf("strings: %s\n", strings)
+			continue
+		}
+		if ints, ok := shuju.([]int); ok {
+			fmt.Printf("ints: %v\n", ints)
+			continue
+		} else {
+			fmt.Printf("type=%T;%v", shuju, &shuju)
+		}
+		defer fmt.Printf("\n")
+	}
+}
